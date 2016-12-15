10d95a3221adff161682eb311ac5d6cd38afe051
cfu12 <cfu@walmartlabs.com>
2016-12-15 13:06:56 -0800

    customization

--- app.js
+++ app.js
@@ -23,29 +23,40 @@ let bodyParser = require('body-parser');
 
 let app = express();
 app.set('port', (process.env.PORT || 8080));
-app.use(bodyParser.json({type: 'application/json'}));
+app.use(bodyParser.json({
+  type: 'application/json'
+}));
 
-app.post('/', function (request, response) {
+
+app.get('/', function(request, response) {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({
+    whoami: david fu
+  }, null, 3));
+});
+
+app.post('/', function(request, response) {
   console.log('handle post');
-  const assistant = new ActionsSdkAssistant({request: request, response: response});
+  const assistant = new ActionsSdkAssistant({
+    request: request,
+    response: response
+  });
 
-  function mainIntent (assistant) {
+  function mainIntent(assistant) {
     console.log('mainIntent');
-    let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
-          'I can read out an ordinal like ' +
-          '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
-          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! This is David Fu <break time="1"/> ' +
+      'I can read out an ordinal like ' +
+      '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>', ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
     assistant.ask(inputPrompt);
   }
 
-  function rawInput (assistant) {
+  function rawInput(assistant) {
     console.log('rawInput');
     if (assistant.getRawInput() === 'bye') {
       assistant.tell('Goodbye!');
     } else {
       let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
-        assistant.getRawInput() + '</say-as></speak>',
-          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+        assistant.getRawInput() + '</say-as></speak>', ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
       assistant.ask(inputPrompt);
     }
   }
@@ -58,7 +69,7 @@ app.post('/', function (request, response) {
 });
 
 // Start the server
-let server = app.listen(app.get('port'), function () {
+let server = app.listen(app.get('port'), function() {
   console.log('App listening on port %s', server.address().port);
   console.log('Press Ctrl+C to quit.');
 });
